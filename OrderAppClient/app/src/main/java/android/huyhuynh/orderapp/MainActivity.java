4dc85a77d605d9da481c49a8b63f631ec028eae1
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-08-20 23:54:27 +0700

    Design main activity

--- OrderAppClient/app/src/main/java/android/huyhuynh/orderapp/MainActivity.java
+++ OrderAppClient/app/src/main/java/android/huyhuynh/orderapp/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.huyhuynh.orderapp.views.MenuActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -16,20 +18,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton btnImgQuetma;
+    IntentIntegrator intentIntegrator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Ánh xạ
+        btnImgQuetma = findViewById(R.id.btnImgQuetma);
 
-        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator = new IntentIntegrator(this);
         intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE); //Kiểu quét
         intentIntegrator.setPrompt("Quét mã QR trên bàn!"); // dòng chữ tr
         intentIntegrator.setOrientationLocked(false);
         intentIntegrator.setBeepEnabled(false); //Tiếng kêu sau khi quét xong
         intentIntegrator.setBarcodeImageEnabled(true);
         intentIntegrator.setTimeout(3000); //Thời gian cho đến khi kết thúc
-        intentIntegrator.initiateScan();
+        //intentIntegrator.initiateScan();
     }
 
 
@@ -46,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
                 finish();*/
                 Intent intent = new Intent(MainActivity.this, MenuActivity.class);
                 startActivity(intent);
-                finish();
             } else {
                 try {
                     JSONObject jsonObject = new JSONObject(intentResult.getContents());
@@ -70,4 +74,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void loadQRCamera(View view) {
+        intentIntegrator.initiateScan();
+    }
 }
