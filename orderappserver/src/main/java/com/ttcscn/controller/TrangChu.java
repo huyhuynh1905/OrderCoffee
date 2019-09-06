bc102cf461c25f5c3e38d7607ecef90320e000b6
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-09-06 21:12:30 +0700

    server receive image from android retrofit

--- orderappserver/src/main/java/com/ttcscn/controller/TrangChu.java
+++ orderappserver/src/main/java/com/ttcscn/controller/TrangChu.java
@@ -1,15 +1,40 @@
 package com.ttcscn.controller;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ttcscn.model.MyFile;
 
 @Controller
 public class TrangChu {
 	
-	@RequestMapping("/trangchu")
+	
+	@RequestMapping("/")
 	@ResponseBody
 	public String trangChu() {
 		return "Test";
 	}
+	
+	@RequestMapping(value = "/uploadanh", method = RequestMethod.POST)
+	@ResponseBody
+	public String uploadFile(@RequestParam("imageFile") MultipartFile imageFile) {
+	    System.out.println("Chay duoc");
+	    try {
+	      String fileName = imageFile.getOriginalFilename();
+	      File file = new File("D:\\Acount", fileName);
+	      imageFile.transferTo(file);
+	    } catch (Exception e) {
+	    	System.out.println("Uploadddd Errrorrrr");
+	      //e.printStackTrace();
+	    }
+	    System.out.println("Uploadddd");
+	    return "result_ok";
+	  }
 }
